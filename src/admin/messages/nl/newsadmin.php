9be95981e085d8ef6e841b84cc2f159b3f086426
Maarten van Middelaar <mahkali@gmail.com>
2018-08-12 00:50:19 +0200

    Add dutch language

---
+++ src/admin/messages/nl/newsadmin.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    // added translation in 1.0.0-beta5: 
+    'news' => 'Nieuws',
+    'news_administrate' => 'Beheer',
+    'article' => 'Nieuwsbericht',
+    'article_cat_id' => 'Categorie',
+    'article_title' => 'Titel',
+    'article_text' => 'Inhoud',
+    'article_image_id' => 'Afbeelding',
+    'article_timestamp_create' => 'Datum',
+    'article_timestamp_display_from' => 'Weergave van',
+    'article_timestamp_display_until' => 'Display tot',
+    'article_is_display_limit' => 'Weergave beperken',
+    'article_image_list' => 'Afbeeldingenlijst',
+    'article_file_list' => 'Bestandslijst',
+    'article_tag' => 'Tag',
+    'kat' => 'Categorieën',
+    'cat_title' => 'Categorie',
+    'cat_delete_error' => 'Deze categorie wordt gebruikt door één of meerdere nieuwsberichten en kan niet worden verwijderd.',
+    'cat_title_create_error' => 'Voeg een categorietitel toe.',
+    'tag' => 'Tags',
+    'tag_title' => 'Tag',
+    'tag_title_create_error' => 'Voeg een tagtitel toe.',
+
+    // 1.0.0 
+
+    'teaser_text' => 'Teasertekst',
+];
