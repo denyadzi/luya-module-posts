a5a94a2c4310880f4173503c47e5cea883586ac2
Dzianis Jackievič <den.overtone@gmail.com>
2019-06-14 15:48:59 +0300

    use new admin release and crud customization

--- src/admin/controllers/ArticleController.php
+++ src/admin/controllers/ArticleController.php
@@ -9,37 +9,7 @@ use luya\posts\admin\render\RenderArticleCrud;
 class ArticleController extends \luya\admin\ngrest\base\Controller
 {
     public $modelClass = '\luya\posts\models\Article';
-
-    /**
-     * Overriden just to use custom RenderCrud.
-     * @TODO `renderCrud` controller property
-     * 
-     * @inheritdoc
-     */
-    public function actionIndex($inline = false, $relation = false, $arrayIndex = false, $modelClass = false, $modelSelection = false)
-    {
-        $apiEndpoint = $this->model->ngRestApiEndpoint();
-
-        $config = $this->model->getNgRestConfig();
-
-        $userSortSettings = Yii::$app->adminuser->identity->setting->get('ngrestorder.admin/'.$apiEndpoint, false);
-        
-        if ($userSortSettings && is_array($userSortSettings) && $config->getDefaultOrder() !== false) {
-            $config->defaultOrder = [$userSortSettings['field'] => $userSortSettings['sort']];
-        }
-        
-        // generate crud renderer
-        $crud = new RenderArticleCrud();
-        $crud->setModel($this->model);
-        $crud->setSettingButtonDefinitions($this->globalButtons);
-        $crud->setIsInline($inline);
-        $crud->setModelSelection($modelSelection);
-        if ($relation && is_numeric($relation) && $arrayIndex !== false && $modelClass !== false) {
-            $crud->setRelationCall(['id' => $relation, 'arrayIndex' => $arrayIndex, 'modelClass' => $modelClass]);
-        }
-        
-        // generate ngrest object from config and render renderer
-        $ngrest = new NgRest($config);
-        return $ngrest->render($crud);
-    }
+    public $renderCrud = [
+        'view' => '\luya\posts\admin\render\RenderArticleCrudView',
+    ];
 }
--- src/admin/render/RenderArticleCrudView.php
+++ src/admin/render/RenderArticleCrudView.php
@@ -14,31 +14,7 @@ class RenderArticleCrudView extends \luya\admin\ngrest\render\RenderCrudView
      */
     public function registerAngularControllerScript()
     {
-        $config = [
-            'apiListQueryString' => $this->context->apiQueryString('list'),
-            'apiUpdateQueryString' => $this->context->apiQueryString('update'),
-            'apiEndpoint' => $this->context->getApiEndpoint(),
-            'list' => $this->context->getFields('list'),
-            'create' => $this->context->getFields('create'),
-            'update' => $this->context->getFields('update'),
-            'ngrestConfigHash' => $this->context->getConfig()->getHash(),
-            'activeWindowCallbackUrl' => $this->context->getApiEndpoint('active-window-callback'),
-            'activeWindowRenderUrl' =>  $this->context->getApiEndpoint('active-window-render'),
-            'pk' => $this->context->getConfig()->getPrimaryKey(),
-            'inline' => $this->context->getIsInline(),
-            'modelSelection' => $this->context->getModelSelection(),
-            'orderBy' => $this->context->getOrderBy(),
-            'tableName' => $this->context->getConfig()->getTableName(),
-            'groupBy' => $this->context->getConfig()->getGroupByField() ? 1 : 0,
-            'groupByField' => $this->context->getConfig()->getGroupByField() ? $this->context->getConfig()->getGroupByField() : '0',
-            'groupByExpanded' => $this->context->getConfig()->getGroupByExpanded(),
-            'filter' => '0',
-            'fullSearchContainer' => false,
-            'minLengthWarning' => false,
-            'saveCallback' => $this->context->getConfig()->getOption('saveCallback') ? new JsExpression($this->context->getConfig()->getOption('saveCallback')) : false,
-            'relationCall' => $this->context->getRelationCall(),
-            'relations' => $this->context->getConfig()->getRelations(),
-        ];
+        $config = $this->getAngularControllerConfig();
         
         $client = 'zaa.bootstrap.register("'.$this->context->config->getHash().'", ["$scope", "$controller", "autopostQueueWorker", function($scope, $controller, autopostQueueWorker) {
 			$.extend(this, $controller("CrudController", { $scope : $scope }));
